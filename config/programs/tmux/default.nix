f69094e356f6c99d38621ede96322c2338e78778
rbxStardev <miguelscb0310@gmail.com>
2026-04-29 21:37:21 -0300

    Make tmux-menus plugin source

--- config/programs/tmux/default.nix
+++ config/programs/tmux/default.nix
@@ -1,4 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  tmux-menus = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-menus";
+    version = "unstable-2025-04-29";
+    src = pkgs.fetchFromGitHub {
+      owner = "jaclu";
+      repo = "tmux-menus";
+      rev = "77a95a7e31fe26ab9615f3f734c793fe436ec219";
+      hash = "sha256-pqm2CFnaOdi9fpU85uoQoA/V3925liqwElcd/N7LAtQ=";
+    };
+  };
+in {
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
@@ -11,7 +26,7 @@
     clock24 = true;
     historyLimit = 50000;
 
-    plugins = with pkgs; [tmuxPlugins.better-mouse-mode];
+    plugins = with pkgs; [tmuxPlugins.better-mouse-mode tmux-menus];
 
     extraConfig = ''
       set -g default-terminal "xterm-256color"
