3622220736fd88ac56a701227f2f42bd542d0156
rbxStardev <miguelscb0310@gmail.com>
2026-05-02 21:44:34 -0300

    plugin tweak

--- config/sessions/hyprland/plugins/hypr-dynamic-cursors.nix
+++ config/sessions/hyprland/plugins/hypr-dynamic-cursors.nix
@@ -3,21 +3,26 @@
     plugins = [pkgs.hyprlandPlugins.hypr-dynamic-cursors];
 
     settings = {
-      "plugin:dynamic-cursors" = {
-        enabled = true;
-
-        mode = "stretch";
-        stretch = {
-          limit = 3000;
-          function = "quadratic";
-          window = 100;
-        };
-
-        hyprcursor = {
-          nearest = true;
+      plugin = {
+        dynamic-cursors = {
           enabled = true;
-          resolution = -1;
-          fallback = "clientside";
+
+          mode = "stretch";
+          stretch = {
+            limit = 3000;
+            function = "quadratic";
+            window = 100;
+          };
+          shake = {
+            enabled = false;
+          };
+
+          hyprcursor = {
+            nearest = true;
+            enabled = true;
+            resolution = -1;
+            fallback = "clientside";
+          };
         };
       };
     };
